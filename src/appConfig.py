e2ef42ee138e4be6590c55901102a32d36283d76
Seth Wilfinger <130659278+WilfingerS@users.noreply.github.com>
2026-05-09 06:44:17 -0400

    some changes

--- src/appConfig.py
+++ src/appConfig.py
@@ -3,6 +3,7 @@ class AppConfig:
         self.name = "Mutation Appraiser"
         self.version = "1.0"
         self.lang = "EN"
+        self.author = "Plum (smolploom)"
 
     def get_title(self):
         return f"{self.name} v{self.version} ({self.lang})"
